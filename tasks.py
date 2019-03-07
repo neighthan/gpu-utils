5fb9373d1495eb0133101f4e48eccd85909febb9
Nathan Hunt <neighthan.hunt@gmail.com>
2019-03-07 12:07:21 -0500

    Update tasks.

--- tasks.py
+++ tasks.py
@@ -1,106 +1,221 @@
+"""
+Assumptions:
+* the project is structured as <name>/<name'> where <name>
+  is the project name and <name'> is the same as <name> except with any "-"
+  replaced by "_". `tasks.py` should be stored in <name>.
+  If you cloned this from gh:neighthan/cookiecutter-pytemplate, the structure is right.
+"""
+
 import re
+import shutil
+import sys
+import json
+import toml
+import invoke
 from pathlib import Path
 from time import sleep
-from invoke import task
+from typing import Sequence
+from urllib.request import urlopen
+from urllib.parse import quote
 from invoke.exceptions import UnexpectedExit
 
+_version_pattern = re.compile(
+    r"(?P<major>\d+)\.(?P<minor>\d+)\.(?P<micro>\d+)(\.(?P<suffix>[A-z0-9]+))?"
+)
+_index_url = "--index-url https://test.pypi.org/simple"
+_extra_url = "--extra-index-url https://pypi.org/simple"  # for dependencies
 
-@task
-def upload(ctx, test: bool = False, n_download_tries: int = 3):
+
+@invoke.task
+def clean(ctx) -> None:
     """
-    Assumptions:
-    * the project is structured as <name>/<name'> where <name>
-      is the project name and <name'> is the same as <name> except with any "-"
-      replaced by "_". `tasks.py` should be stored in <name>.
-    * the version is stored in <name>/<name'>/_version.py and that
-      this file only contains `__version__ = "<version>"` where <version> is of the
-      format major.minor.micro (with optional suffix such as dev0).
-    * If you use the test flag, you have at least the following in `~/.pypirc`:
+    Remove all temporary files and directories.
+
+    Files / directories removed are:
+      * __pycache__ directories
+      * .coverage files
+      * build and dist directories
+    """
+
+    root_dir = Path(__file__).parent
+
+    rm_file_patterns = [".coverage"]
+
+    for file_pattern in rm_file_patterns:
+        for rm_file in root_dir.rglob(file_pattern):
+            rm_file.unlink()
+
+    rm_dir_patterns = ["__pycache__", "build", "dist"]
+
+    for dir_pattern in rm_dir_patterns:
+        for rm_dir in root_dir.rglob(dir_pattern):
+            shutil.rmtree(str(rm_dir))
+
+
+@invoke.task(clean, post=[clean])
+def publish(
+    ctx, test: bool = False, install: bool = False, n_download_tries: int = 3
+) -> None:
+    """
+    Publish the project to pypi / testpypi.
+
+    If you use the test flag, you have at least the following in `~/.pypirc`:
       [testpypi]
       repository: https://test.pypi.org/legacy/
+
+    :param ctx: invoke context
+    :param test: whether to publish to normal or test pypi. If publishing to testpypi,
+      .dev<dev_num> is added to the version where <dev_num> is one larger than the
+      highest dev version published. This is because testpypi won't let you publish the
+      same version multiple times; doing this automates changing the version for repeat
+      publishing + testing.
+      Additionally, the micro/patch version is incremented because it's assumed that
+      it's a dev version of the _next_ release.
+      WARNING - don't publish multiple times too quickly. If so, the next dev num
+      can't be pulled from testpypi because it won't have updated yet.
+    :param install: whether to install the project from test pypi.
+      Only used if `test` is true. This is better than running `invoke install`
+      separately because it will try multiple times to get the newly uploaded version
+      (it usually takes a couple of tries).
+    :param n_download_tries: how many times to attempt to install the project.
+      After each attempt there is a 5 second sleep period.
     """
 
-    # TODO - in case version has a suffix besides dev, we should read the whole suffix
-    # replace it by dev<dev_num>, do the test, then write back the original suffix
-    # again. If the suffix isn't dev, though, we won't know what to set dev_num to
-    # so we'll need to run pip to check what the latest version is that has the same
-    # major.minor.micro and dev; then we can pull dev_num from that and increment
-    # pip install --index-url https://test.pypi.org/simple/ gpu-utils==? then use re
-    # to parse out all versions; make a pattern from the regex below so you can use
-    # it in both places.
-
-    # TODO - don't upload if test is False and there are unstaged changes to
-    # tracked files unless --force is given
-
-    # TODO - add a git tag to the most recent commit that says what the version is
-    # https://stackoverflow.com/questions/4404172/how-to-tag-an-older-commit-in-git
-
-    # TODO - put this into a separate repo that you use as a template for python
-    # projects
-
-    project_root = "/cluster/nhunt/github/gpu-utils"
-    project_name = "gpu-utils"
+    project_name = _get_from_pyproject(["tool", "poetry", "name"])
+    project_root = str(Path(__file__).parent.resolve())
     sleep_time = 5
 
-    version = {}
-    version_path = (
-        Path(__file__).parent / project_name.replace("-", "_") / "_version.py"
-    )
-    exec(version_path.read_text(), version)
-    version = version["__version__"]
-
     if test:
-        # add dev if lacking; increment dev number if present
-        # this is because test.pypi still won't let you upload the same version
-        # multiple times. Doing this automates changing the version for repeat testing
+        pyproject_path = Path(__file__).parent / "pyproject.toml"
+        original_pyproject_str = pyproject_path.read_text()
+        pyproject = toml.loads(original_pyproject_str)
+        original_version = pyproject["tool"]["poetry"]["version"]
 
-        versions = re.fullmatch(
-            r"(?P<major>\d+)\.(?P<minor>\d+)\.(?P<micro>\d+)(dev(?P<dev_num>\d+))?",
-            version,
-        )
-        dev_num = versions.groupdict()["dev_num"]
-        if dev_num:
-            version = version.replace(f"dev{dev_num}", f"dev{int(dev_num) + 1}")
-        else:
-            version += "dev0"
-    else:
-        # remove dev if present
-        try:
-            version = version[: version.index("dev")]
-        except ValueError:
-            pass
+        version = re.fullmatch(_version_pattern, original_version)
+        groups = version.groupdict()
+        major, minor, micro = groups["major"], groups["minor"], groups["micro"]
+        version = f"{major}.{minor}.{int(micro) + 1}"
+        dev_num = _get_next_dev_num(project_name, version)
+        version += f".dev{dev_num}"
 
-    # write back the modified version
-    version_path.write_text(f'__version__ = "{version}"\n')
+        # write back the modified version
+        pyproject["tool"]["poetry"]["version"] = version
+        pyproject_path.write_text(toml.dumps(pyproject))
 
-    cmd = f"""
-    cd {project_root}
+    try:
+        cmd = f"""
+        cd "{project_root}"
 
-    rm -rf build
-    rm -rf dist
+        poetry build
+        twine upload {'--repository testpypi' if test else ''} dist/*
+        """
 
-    pip install -U setuptools wheel twine
-    python setup.py sdist bdist_wheel
-    twine upload {'--repository testpypi' if test else ''} dist/*
+        ctx.run(cmd)
+    finally:
+        if test:
+            pyproject_path.write_text(original_pyproject_str)
 
-    rm -rf build
-    rm -rf dist
-    """
-
-    ctx.run(cmd)
-
-    if not test:
+    if not test or not install:
         return
 
     for i in range(n_download_tries):
         sleep(sleep_time)
         try:
             result = ctx.run(
-                f"""
-            cd
-            pip install --index-url https://test.pypi.org/simple/ {project_name}=={version}
-            """
+                f"pip install {_index_url} {_extra_url} {project_name}=={version}"
             )
             break
         except UnexpectedExit:
             continue
+
+
+@invoke.task
+def update_tasks(ctx) -> None:
+    """
+    Update the tasks file to the newest version on GitHub.
+
+    :param ctx: invoke context
+    """
+
+    tasks_path = Path(__file__).resolve()
+    github_url = "https://raw.githubusercontent.com/neighthan/cookiecutter-pytemplate/"
+    github_url += quote("master/{{cookiecutter.project_name}}/tasks.py")
+
+    with urlopen(github_url) as new_tasks_file:
+        tasks_path.write_text(new_tasks_file.read().decode())
+
+
+@invoke.task
+def install(ctx, version: str = "", test: bool = False) -> None:
+    """
+    Install the latest version of the current project.
+
+    :param ctx: `invoke` context
+    :param test: whether to install from test pypi;
+      if so, `--pre` is used to allow dev versions
+    """
+
+    project_name = _get_from_pyproject(["tool", "poetry", "name"])
+    cmd = "pip install -U {} " + project_name + f"=={version}" if version else ""
+    cmd = cmd.format(" ".join([_index_url, _extra_url, "--pre"]) if test else "")
+    ctx.run(cmd)
+
+
+@invoke.task(clean, allow_unknown=True)
+def test(ctx) -> None:
+    pytest_args = sys.argv[sys.argv.index("test") + 1 :]
+    for i, arg in enumerate(pytest_args):
+        if " " in arg:
+            pytest_args[i] = f'"{arg}"'
+    cmd = "poetry run pytest " + " ".join(pytest_args)
+    ctx.run(cmd, pty=True)
+
+
+@invoke.task
+def install_jupyter_kernel(ctx, name: str, install_prefix: str = "") -> None:
+    """
+    :param name: name for the kernel
+    :param install_prefix: default = ~/.local
+    """
+    install_prefix = Path.home() / ".local"
+    cmd = f'poetry run python -m ipykernel install --prefix={install_prefix} --name "{name}"'
+    ctx.run(cmd)
+
+
+def _get_next_dev_num(project_name: str, current_version: str) -> int:
+    """
+    Get 1 + the number of the latest dev version matching `current_version` w/o suffix.
+
+    To determine next dev_num we run pip and find the latest version that has the same
+    major.minor.micro as `current_version` and dev in the suffix, then we increment.
+    """
+
+    cmd = f"pip install {_index_url} {project_name}==?"
+    result = invoke.run(cmd, warn=True, hide=True)
+
+    current_version = re.fullmatch(_version_pattern, current_version)
+    current_version_groups = current_version.groupdict()
+
+    dev_num = 0
+    # reverse so that we hit the latest version first
+    for published_version in list(re.finditer(_version_pattern, result.stderr))[::-1]:
+        groups = published_version.groupdict()
+        if (
+            groups["major"] == current_version_groups["major"]
+            and groups["minor"] == current_version_groups["minor"]
+            and groups["micro"] == current_version_groups["micro"]
+            and groups["suffix"]
+            and groups["suffix"].startswith("dev")
+        ):
+            dev_num = int(groups["suffix"].replace("dev", "")) + 1
+            break
+    return dev_num
+
+
+def _get_from_pyproject(keys: Sequence[str]):
+    pyproject = Path(__file__).parent / "pyproject.toml"
+    pyproject = toml.loads(pyproject.read_text())
+    ret = pyproject
+    for key in keys:
+        ret = ret[key]
+    return ret
